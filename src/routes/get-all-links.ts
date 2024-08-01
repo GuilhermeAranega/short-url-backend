eb0dd47403030bb3a549ce7aeb2e73ee44513024
Guilherme Aranega <guilhermee.aranega@gmail.com>
2024-08-01 19:23:24 -0300

    feat: add get all shorted links by user route

---
+++ src/routes/get-all-links.ts
@@ -0,0 +1,61 @@
+import { prisma } from "../lib/prisma";
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { JWTPayload } from "../utils/jwt-payload";
+import { verifyJWT } from "../middleware/JWT";
+
+export async function getAllLinks(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    "/links",
+    {
+      schema: {
+        querystring: z.object({
+          pageIndex: z.string().nullish().default("0").transform(Number),
+        }),
+        response: {
+          201: z.object({
+            message: z.string(),
+            links: z
+              .object({
+                id: z.string().cuid(),
+                title: z.string(),
+                url: z.string().url(),
+                shortedUrl: z.string().url(),
+              })
+              .array(),
+          }),
+        },
+      },
+    },
+    async (req, res) => {
+      const { pageIndex } = req.query;
+      const tokenData = (await verifyJWT(req, res)) as JWTPayload;
+
+      const user = await prisma.users.findUnique({
+        where: { id: tokenData.userId },
+      });
+
+      if (!user) {
+        throw new Error("User not found");
+      }
+
+      const links = await prisma.links.findMany({
+        where: { usersId: tokenData.userId },
+        select: {
+          id: true,
+          title: true,
+          url: true,
+          shortedUrl: true,
+        },
+        take: 10,
+        skip: pageIndex * 10,
+      });
+
+      return res.status(201).send({
+        message: "Shorted url created",
+        links,
+      });
+    }
+  );
+}
