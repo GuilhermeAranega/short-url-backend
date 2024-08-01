a06d55460f88af05ee9d218c47e7806279296f33
Guilherme Aranega <guilhermee.aranega@gmail.com>
2024-08-01 18:48:12 -0300

    feat: add redirect route

---
+++ src/routes/redirect-link.ts
@@ -0,0 +1,36 @@
+import { prisma } from "../lib/prisma";
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { link } from "fs";
+import { z } from "zod";
+
+export async function redirectLink(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    "/r/:slug",
+    {
+      schema: {
+        params: z.object({
+          slug: z.string(),
+        }),
+      },
+    },
+    async (req, res) => {
+      const { slug } = req.params;
+
+      const link = await prisma.links.findUnique({
+        where: { slug },
+      });
+
+      if (!link) {
+        throw new Error("Link not found");
+      }
+
+      await prisma.links.update({
+        where: { slug },
+        data: { clicks: { increment: 1 } },
+      });
+
+      return res.redirect(link.url, 301);
+    }
+  );
+}
