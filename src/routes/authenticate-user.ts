35eb968dffe73dcebe556d21ac22b5c7c1e964f8
Guilherme Aranega <guilhermee.aranega@gmail.com>
2024-07-31 23:08:21 -0300

    feat: add a route to authenticate the user

---
+++ src/routes/authenticate-user.ts
@@ -0,0 +1,52 @@
+import { prisma } from "../lib/prisma";
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+
+export async function authenticateUser(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/auth/:id",
+    {
+      schema: {
+        params: z.object({
+          id: z.string().cuid(),
+        }),
+        response: {
+          201: z.object({
+            message: z.string(),
+          }),
+        },
+      },
+    },
+    async (req, res) => {
+      const { id } = req.params;
+
+      const token = await prisma.auth.findUnique({
+        where: { id },
+      });
+
+      if (!token) {
+        throw new Error("Token not found");
+      }
+
+      if (token.successful) {
+        throw new Error("Link already used");
+      }
+
+      res.setCookie("token", token.token, {
+        path: "/",
+        httpOnly: true,
+        secure: true,
+      });
+
+      await prisma.auth.update({
+        where: { id },
+        data: { successful: true },
+      });
+
+      return res.status(201).send({
+        message: "Login successful",
+      });
+    }
+  );
+}
